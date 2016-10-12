nigelzor/yarn

commit 369539f7553c74deb04c080cc140187eeb2c5f5e

Fix custom registries with paths (#712)
Ensures with and without trailing / registry url is correct and resolvable.

src/registries/npm-registry.js
@@ -54,8 +54,11 @@ export default class NpmRegistry extends Registry {
       headers.authorization = `Bearer ${this.token}`;
     }
 
+    // $FlowFixMe : https://github.com/facebook/flow/issues/908
+    const requestUrl = url.format(`${registry}/${pathname}`);
+
     return this.requestManager.request({
-      url: url.resolve(registry, pathname),
+      url: requestUrl,
       method: opts.method,
       body: opts.body,
       auth: opts.auth,

commit 17e79317b3c802a44b71db3be6b2942482ec2788

Fix requestUrl
* Broken by #712, which changed from url.resolve to url.format. We need resolve in the case where the pathname is really a full url (when requesting tarballs). So only add the trailing / if it is not there.
* Only add authorization if the request is going to the registry (not random tarballs).

src/registries/npm-registry.js
@@ -7,6 +7,7 @@ import type {ConfigRegistries} from './index.js';
 import * as fs from '../util/fs.js';
 import NpmResolver from '../resolvers/registries/npm-resolver.js';
 import Registry from './base-registry.js';
+import {addSuffix} from '../util/misc';
 
 const defaults = require('defaults');
 const userHome = require('user-home');
@@ -49,15 +50,13 @@ export default class NpmRegistry extends Registry {
   }
 
   request(pathname: string, opts?: RegistryRequestOptions = {}): Promise<*> {
-    const registry = this.getRegistry(pathname);
+    const registry = addSuffix(this.getRegistry(pathname), '/');
+    const requestUrl = url.resolve(registry, pathname);
 
     const headers = {};
-    if (this.token || this.getOption('always-auth')) {
+    if (this.token || (this.getOption('always-auth') && requestUrl.startsWith(registry))) {
       headers.authorization = this.getAuth(pathname);
     }
-
-    // $FlowFixMe : https://github.com/facebook/flow/issues/908
-    const requestUrl = url.format(`${registry}/${pathname}`);
 
     return this.requestManager.request({
       url: requestUrl,

src/util/misc.js
@@ -31,6 +31,14 @@ export function removeSuffix(pattern: string, suffix: string): string {
   return pattern;
 }
 
+export function addSuffix(pattern: string, suffix: string): string {
+  if (!pattern.endsWith(suffix)) {
+    return pattern + suffix;
+  }
+
+  return pattern;
+}
+
 export function stringify(obj: Object): string {
   return JSON.stringify(obj, null, '  ');
 }